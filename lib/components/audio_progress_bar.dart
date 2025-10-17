9f75b64d7dc8244733cb2c0dfb0dc8355ecd1eed
nicx519y <nicx519y@gmail.com>
2025-10-17 16:13:42 +0800

    first commit

--- lib/components/audio_progress_bar.dart
+++ lib/components/audio_progress_bar.dart
@@ -349,9 +349,14 @@ class _AudioProgressBarState extends State<AudioProgressBar> {
 
   String _formatDuration(Duration duration) {
     String twoDigits(int n) => n.toString().padLeft(2, '0');
-    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
-    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
-    return "$twoDigitMinutes:$twoDigitSeconds";
+    final hours = duration.inHours;
+    final minutes = duration.inMinutes.remainder(60);
+    final seconds = duration.inSeconds.remainder(60);
+    if (hours > 0) {
+      return "${twoDigits(hours)}:${twoDigits(minutes)}:${twoDigits(seconds)}";
+    } else {
+      return "${twoDigits(minutes)}:${twoDigits(seconds)}";
+    }
   }
 
   @override
